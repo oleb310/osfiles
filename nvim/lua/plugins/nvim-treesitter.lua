c57887cc9d2ea903e011987319c4d1c2ab1102f7
oleb310 <105802171+oleb310@users.noreply.github.com>
2022-10-13 10:07:15 +0200

    Update nvim-treesitter.lua

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,8 @@
 -- nvim-treesitter configuration
+
 local status_ok, nvim_treesitter = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then
-  return
+    return
 end
 
 nvim_treesitter.setup {
